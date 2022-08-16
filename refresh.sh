5bca3e37028e2eecdbafd420a181102feb5d960f
Brian H. Ward <glyphrider@gmail.com>
2022-08-16 08:43:38 -0400

    a useful note

--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,6 @@ DATE=`date +%Y%m%d`
 
 docker build -t glyphrider/cpp:latest -t glyphrider/cpp:$DATE .
 
+# the push will requre a successful docker login
 docker push glyphrider/cpp:$DATE
 docker push glyphrider/cpp:latest
